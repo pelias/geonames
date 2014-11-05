5bdcfeb749fe913610f1c64d6235eff6a6d4b651
Peter Johnson <insomnia@rcpt.at>
2014-11-05 15:41:03 +0000

    refactor, see PR notes

--- task/import.js
+++ task/import.js
@@ -2,58 +2,85 @@
 var geonames = require('geonames-stream'),
     suggester = require('pelias-suggester-pipeline'),
     through = require('through2'),
-    esclient = require('pelias-esclient')({ throttle: 10, maxThrottle: 50 }),
-    Backend = require('geopipes-elasticsearch-backend'),
-    elasticsearch = new Backend( esclient, 'pelias', 'geoname' ),
     resolvers = require('./resolvers'),
     propStream = require('prop-stream'),
     schema = require('pelias-schema');
 
 function mapper( data, enc, next ){
 
-  var record = {
-    id: data._id,
-    _meta: {
-      type: 'geoname'
-    },
-    name: {
-      default: data.name.trim()
-    },
-    admin0: resolvers.country_name(data.country_code),
-    admin1: resolvers.admin1_name(data),
-    admin2: resolvers.admin2_name(data),
-    center_point: {
-      lat: data.latitude,
-      lon: data.longitude
-    }
-  };
+  try {
+    var record = {
+      id: data._id,
+      _meta: {
+        type: 'geoname'
+      },
+      name: {
+        default: data.name.trim()
+      },
+      admin0: resolvers.country_name(data.country_code),
+      admin1: resolvers.admin1_name(data),
+      admin2: resolvers.admin2_name(data),
+      center_point: {
+        lat: data.latitude,
+        lon: data.longitude
+      }
+    };
 
-  // alternate names
-  // @see https://github.com/mapzen/pelias-geonames/issues/5
-  // data.alternatenames.forEach( function( name, i ){
-  //   record.name[ 'alt'+i ] = name;
-  // });
+    // alternate names
+    // @see https://github.com/mapzen/pelias-geonames/issues/5
+    // data.alternatenames.forEach( function( name, i ){
+    //   record.name[ 'alt'+i ] = name;
+    // });
 
-  this.push(record);
-  next();
+    this.push(record);
+  } catch( e ){
+    console.error( 'mapper failure', e );
+    process.exit(1);
+  } finally {
+    next();
+  }
 }
 
 module.exports = function( filename ){
 
-  // show es stats
-  esclient.livestats();
-
   // remove any props not in the geonames mapping
   var allowedProperties = Object.keys( schema.mappings.geoname.properties ).concat( [ 'id', 'type' ] );
 
+  var counts = {};
+  var countStream = function( key, showIds ){
+    return through.obj( function( item, enc, next ){
+      if( !counts.hasOwnProperty( key ) ){ counts[key] = 0; }
+      ++counts[key];
+      console.log( 'counts', counts );
+      this.push( item );
+      next();
+    });
+  };
+
+  var devnull = through.obj( function( item, enc, next ){
+    next();
+  });
+
   // run import pipeline
-  resolvers.selectSource( filename )
+resolvers.selectSource( filename )
     .pipe( geonames.pipeline )
     .pipe( through.obj( mapper ) )
     .pipe( suggester.pipeline )
     .pipe( propStream.whitelist( allowedProperties ) )
-    .pipe( elasticsearch.createPullStream() );
-    // .pipe( geonames.stringify )
-    // .pipe( process.stdout );
+    .pipe( propStream.blacklist( ['tags'] ) )
+    .pipe( through.obj( function( item, enc, next ){
+      
+      var id = item.id;
+      delete item.id;
 
+      this.push({
+        _index: 'pelias',
+        _type: 'geoname',
+        _id: id,
+        data: item
+      });
+
+      next();
+    }))
+    .pipe( require('dbclient')() );
 };
