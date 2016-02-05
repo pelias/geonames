dea0e6c55bc3e7fa7ea5e0c19d19e54a22d6e888
Stephen Hess <stephen.k.hess@gmail.com>
2016-02-05 12:06:18 -0500

    renamed `createPeliasDocGenerator` to `create` for convention

--- test/streams/peliasDocGeneratorTest.js
+++ test/streams/peliasDocGeneratorTest.js
@@ -24,7 +24,7 @@ tape('peliasDocGenerator', function(test) {
       .setName('default', 'Record Name')
       .setCentroid({ lat: 12.121212, lon: 21.212121 });
 
-    var docGenerator = peliasDocGenerator.createPeliasDocGenerator();
+    var docGenerator = peliasDocGenerator.create();
 
     test_stream([input], docGenerator, function(err, actual) {
       t.deepEqual(actual, [expected], 'should have returned true');
@@ -40,7 +40,7 @@ tape('peliasDocGenerator', function(test) {
       longitude: 21.212121
     };
 
-    var docGenerator = peliasDocGenerator.createPeliasDocGenerator();
+    var docGenerator = peliasDocGenerator.create();
 
     test_stream([input], docGenerator, function(err, actual) {
       t.deepEqual(actual, [], 'should have returned true');
@@ -66,7 +66,7 @@ tape('peliasDocGenerator', function(test) {
       .addCategory('government')
       .addCategory('natural');
 
-    var docGenerator = peliasDocGenerator.createPeliasDocGenerator();
+    var docGenerator = peliasDocGenerator.create();
 
     test_stream([input], docGenerator, function(err, actual) {
       t.deepEqual(actual, [expected], 'should have returned true');
@@ -89,7 +89,7 @@ tape('peliasDocGenerator', function(test) {
       .setCentroid({ lat: 12.121212, lon: 21.212121 })
       .setMeta('fcode', 'Unsupported feature_code');
 
-    var docGenerator = peliasDocGenerator.createPeliasDocGenerator();
+    var docGenerator = peliasDocGenerator.create();
 
     test_stream([input], docGenerator, function(err, actual) {
       t.deepEqual(actual, [expected], 'should have returned true');
@@ -113,7 +113,7 @@ tape('peliasDocGenerator', function(test) {
       .setAlpha3('USA')
       .setAdmin('admin0', 'United States');
 
-    var docGenerator = peliasDocGenerator.createPeliasDocGenerator();
+    var docGenerator = peliasDocGenerator.create();
 
     test_stream([input], docGenerator, function(err, actual) {
       t.deepEqual(actual, [expected], 'should have returned true');
@@ -135,7 +135,7 @@ tape('peliasDocGenerator', function(test) {
       .setName('default', 'Record Name')
       .setCentroid({ lat: 12.121212, lon: 21.212121 });
 
-    var docGenerator = peliasDocGenerator.createPeliasDocGenerator();
+    var docGenerator = peliasDocGenerator.create();
 
     test_stream([input], docGenerator, function(err, actual) {
       t.deepEqual(actual, [expected], 'should have returned true');
@@ -161,7 +161,7 @@ tape('peliasDocGenerator', function(test) {
       .setAdmin('admin0', 'United States')
       .setAdmin('admin1', 'Arkansas');
 
-    var docGenerator = peliasDocGenerator.createPeliasDocGenerator();
+    var docGenerator = peliasDocGenerator.create();
 
     test_stream([input], docGenerator, function(err, actual) {
       t.deepEqual(actual, [expected], 'should have returned true');
@@ -186,7 +186,7 @@ tape('peliasDocGenerator', function(test) {
       .setAlpha3('USA')
       .setAdmin('admin0', 'United States');
 
-    var docGenerator = peliasDocGenerator.createPeliasDocGenerator();
+    var docGenerator = peliasDocGenerator.create();
 
     test_stream([input], docGenerator, function(err, actual) {
       t.deepEqual(actual, [expected], 'should have returned true');
@@ -209,7 +209,7 @@ tape('peliasDocGenerator', function(test) {
       .setName('default', 'Record Name')
       .setCentroid({ lat: 12.121212, lon: 21.212121 });
 
-    var docGenerator = peliasDocGenerator.createPeliasDocGenerator();
+    var docGenerator = peliasDocGenerator.create();
 
     test_stream([input], docGenerator, function(err, actual) {
       t.deepEqual(actual, [expected], 'should have returned true');
@@ -237,7 +237,7 @@ tape('peliasDocGenerator', function(test) {
       .setAdmin('admin1', 'Arkansas')
       .setAdmin('admin2', 'Ashley County');
 
-    var docGenerator = peliasDocGenerator.createPeliasDocGenerator();
+    var docGenerator = peliasDocGenerator.create();
 
     test_stream([input], docGenerator, function(err, actual) {
       t.deepEqual(actual, [expected], 'should have returned true');
@@ -265,7 +265,7 @@ tape('peliasDocGenerator', function(test) {
       .setAdmin('admin0', 'United States')
       .setAdmin('admin1', 'Arkansas');
 
-    var docGenerator = peliasDocGenerator.createPeliasDocGenerator();
+    var docGenerator = peliasDocGenerator.create();
 
     test_stream([input], docGenerator, function(err, actual) {
       t.deepEqual(actual, [expected], 'should have returned true');
@@ -288,7 +288,7 @@ tape('peliasDocGenerator', function(test) {
       .setCentroid({ lat: 12.121212, lon: 21.212121 })
       .setPopulation(127);
 
-    var docGenerator = peliasDocGenerator.createPeliasDocGenerator();
+    var docGenerator = peliasDocGenerator.create();
 
     test_stream([input], docGenerator, function(err, actual) {
       t.deepEqual(actual, [expected], 'should have returned true');
@@ -310,7 +310,7 @@ tape('peliasDocGenerator', function(test) {
       .setName('default', 'Record Name')
       .setCentroid({ lat: 12.121212, lon: 21.212121 });
 
-    var docGenerator = peliasDocGenerator.createPeliasDocGenerator();
+    var docGenerator = peliasDocGenerator.create();
 
     test_stream([input], docGenerator, function(err, actual) {
       t.deepEqual(actual, [expected], 'should have returned true');
