5a482dc154aeec72b6f18670418cf500cf0060e4
Stephen Hess <stephen.k.hess@gmail.com>
2016-02-05 12:24:04 -0500

    removed `ext` parameter since it's never used

--- task/resolvers.js
+++ task/resolvers.js
@@ -7,14 +7,13 @@ var fs = require('fs'),
 var settings = require('pelias-config').generate();
 var basepath = settings.imports.geonames.datapath;
 
-module.exports.selectSource = function(filename,ext) {
+module.exports.selectSource = function(filename) {
   var localFileName = util.format( '%s/%s.zip', basepath, filename );
   var remoteFilePath = util.format( 'http://download.geonames.org/export/dump/%s.zip', filename );
-  var opts = ext ? { encoding: 'utf8' } : {};
 
   if( fs.existsSync( localFileName ) ){
     logger.info( 'reading datafile from disk at:', localFileName );
-    return fs.createReadStream( localFileName, opts );
+    return fs.createReadStream( localFileName);
   } else {
     logger.info( 'streaming datafile from:', remoteFilePath );
     return request.get( remoteFilePath );
