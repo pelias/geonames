9f3330727c7295226c10166ea3503f569f504cfb
Tyler Pedelose <pedelose.1@gmail.com>
2017-10-10 15:14:07 -0400

    Updated to pull from geonames.mil sourced from NGA; initial stages of elasticsearch import

--- bin/download_data.js
+++ bin/download_data.js
@@ -7,28 +7,34 @@ const validateISOCode = require('../lib/validateISOCode');
 // use datapath setting from your config file
 const config = require('pelias-config').generate();
 const basepath = config.imports.geonames.datapath;
-const countryCode = validateISOCode(config.imports.geonames.countryCode);
+const isocode = validateISOCode(config.imports.geonames.countryCode);
 
-const filename = countryCode === 'ALL' ? 'allCountries' : countryCode;
+var filenames = [isocode]
+if (isocode === 'ALL') {
+  var filenames = require("./data/isocodes.json")["isocodes"];
+}
 
-const remoteFilePath = `http://download.geonames.org/export/dump/${filename}.zip`;
-const localFileName = `${basepath}/${filename}.zip`;
+for (var i = 0; i < filenames.length; i++) {
+  const filename = filenames[i]
+  const remoteFilePath = `http://geonames.nga.mil/gns/html/cntyfile/${filename}.zip`;
+  const localFileName = `${basepath}/${filename}.zip`;
 
-logger.info( 'downloading datafile from:', remoteFilePath );
+  logger.info( 'downloading datafile from:', remoteFilePath );
 
-const command = `curl ${remoteFilePath} > ${localFileName}`;
+  const command = `curl ${remoteFilePath} > ${localFileName}`;
 
-const job = child_process.exec(command);
+  const job = child_process.exec(command);
 
-job.stdout.on('data', (data) => {
-  process.stdout.write(data);
-});
+  job.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
 
-job.stderr.on('data', (data) => {
-  process.stderr.write(data);
-});
+  job.stderr.on('data', (data) => {
+    process.stderr.write(data);
+  });
 
-job.on('close', (code) => {
-  console.log(`Geonames download finished with exit code ${code}`);
-  process.exitCode = code;
-});
+  job.on('close', (code) => {
+    console.log(`Geonames download finished with exit code ${code}`);
+    process.exitCode = code;
+  });
+}
