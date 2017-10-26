cda3d7c214b6ae8839de9b2a767bfa99558cdc26
Gibran Parvez <gibran.parvez@rgi-corp.com>
2017-10-26 15:37:34 -0400

    Update script

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 if [ $# -eq 0 ]
   then
-    tag='geonames-mil'
+    tag='latest'
   else
     tag=$1
 fi
 
-docker build -t venicegeo:$tag .
+docker build -t venicegeo/geonames-mil:$tag .
